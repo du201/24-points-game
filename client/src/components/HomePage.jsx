6ec32c364756ad5981771e3ce546d902076ee009
Zhengze Gong <zhengzeg@andrew.cmu.edu>
2020-08-11 22:45:03 +0800

    Home page style update

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -20,46 +20,44 @@ function HomePage(props) {
   return (
     <div className="container h-100">
       <div className="row" style={{ height: "15%" }}>
-        <div className="col my-auto">
-          <button
-            className="btn grey-text btn-outline-light"
+        <div className="my-auto">
+          <a id="change-lang"
             onClick={() => changeLang()}
           >
             <FontAwesomeIcon icon={faLanguage} size='3x' />
-          </button>
+          </a>
         </div>
       </div>
-      <div className="row h-25">
-        <div className="col text-center my-auto">
-          <h1 className="title-homepage">24</h1>
-        </div>
-      </div>
-      <div className="row h-25">
+
+      <div className="row" style={{ height: "75%" }}>
         <div className="col align-self-start text-center">
+          <div className="title-homepage">
+            24
+          </div>
           <div className="d-inline-flex flex-column">
             <HomePageButton
               onClick={props.pressSolveModeButton}
-              display={t("Solve Mode")}
-            />
-            <HomePageButton
-              onClick={props.pressGameModeButton}
-              display={t("Multiplayer")}
+              display={t("Solver")}
             />
             <HomePageButton
               onClick={props.pressSinglePlayModeButton}
               display={t("Singleplayer")}
             />
+            <HomePageButton
+              onClick={props.pressGameModeButton}
+              display={t("Multiplayer")}
+            />
           </div>
         </div>
       </div>
-      <div className="row h-25"></div>
+
       <div className="row" style={{ height: "10%" }}>
         <div className="col text-center my-auto">
           <div className="d-inline-flex flex-row">
-            <a href="#" className="grey-text no-underline mx-3">{t("ABOUT")}</a>
-            <a href="#" className="grey-text no-underline mx-3">{t("SHARE")}</a>
-            <a href="#" className="grey-text no-underline mx-3">{t("SOURCE")}</a>
-            <a href="#" className="grey-text no-underline mx-3">{t("BUG REPORT")}</a>
+            <a href="#" className="grey-text no-underline mx-4 home-page-link">{t("ABOUT")}</a>
+            <a href="#" className="grey-text no-underline mx-4 home-page-link">{t("SHARE")}</a>
+            <a href="#" className="grey-text no-underline mx-4 home-page-link">{t("SOURCE")}</a>
+            <a href="#" className="grey-text no-underline mx-4 home-page-link">{t("BUG REPORT")}</a>
           </div>
         </div>
       </div>
--- client/src/components/common/HomePageButton.jsx
+++ client/src/components/common/HomePageButton.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 /**
  * the button for the homepage
- * why separate this from normal buttons? 
+ * why separate this from normal buttons?
  * Because home page buttons are fatter, so they need different css
  */
 const HomePageButton = ({ onClick, display }) => {
   return (<button
-    className="btn-outline grey-text btn mb-2"
+    className="btn-outline grey-text btn my-2"
     onClick={onClick}
   >
     {display}
